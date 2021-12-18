577ddc5baaa6df02bf9bf9be6f65e4407408abaa
Luka Liuzzi <lukaliuzzidev@gmail.com>
2021-12-18 03:11:08 -0300

    Modificando la scrollbar

--- js/main.js
+++ js/main.js
@@ -4,23 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function darkMode() {
   const prefiereDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+  const html = document.querySelector("html");
 
   if (prefiereDarkMode.matches) {
     document.body.classList.add("dark-mode");
+    html.classList.add("dark-mode-scrollbar");
   } else {
     document.body.classList.remove("dark-mode");
+    html.classList.remove("dark-mode-scrollbar");
   }
 
   prefiereDarkMode.addEventListener("change", function () {
     if (prefiereDarkMode.matches) {
       document.body.classList.add("dark-mode");
+      html.classList.add("dark-mode-scrollbar");
     } else {
       document.body.classList.remove("dark-mode");
+      html.classList.remove("dark-mode-scrollbar");
     }
   });
 
   const botonDarkMode = document.querySelector(".dark-mode-button");
   botonDarkMode.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
+    html.classList.toggle("dark-mode-scrollbar");
   });
 }
